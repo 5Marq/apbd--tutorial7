8c3ceae016d7bd42dbdd2440f6b838b9f9fba8cb
5Marq <127990910+5Marq@users.noreply.github.com>
2024-06-14 21:02:45 +0200

    direct upload - completed

---
+++ LinqCwiczenia/LinqTutorials/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using LinqTutorials.Models;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var t = LinqTasks.Task1();
+            TestIe(LinqTasks.Task11());
+
+        }
+
+         static void TestIe(IEnumerable<object> list)
+        {
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
